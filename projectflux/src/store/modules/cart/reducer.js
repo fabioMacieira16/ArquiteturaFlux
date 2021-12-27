d9065e71f3b21f84c2a3da8d9554f96b74b91681
Fabio Macieira <fabiomacieira@pmenos.com.br>
2021-12-27 09:40:08 -0300

    separanting actions

--- projectflux/src/store/modules/cart/reducer.js
+++ projectflux/src/store/modules/cart/reducer.js
@@ -4,15 +4,8 @@ export default function cart(state = [], action) {
     switch (action.type) {
         case '@cart/ADD_SUCCESS':
             return produce(state, draft => {
-                const produceIndex = draft.findIndex(p => p.id === action.product.id);
-                if (produceIndex >= 0) {
-                    draft[produceIndex].amount += 1;
-                } else {
-                    draft.push({
-                        ...action.product,
-                        amount: 1,
-                    });
-                }
+                const { product } = action;
+                draft.push(product);
             });
 
         case '@cart/REMOVE':
